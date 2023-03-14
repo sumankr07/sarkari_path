72098c70d53c5cba8a559388a1b0328c1dbc1c40
Suman Kumar <107740609+sumankr07@users.noreply.github.com>
2023-03-14 19:26:59 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,27 +4,27 @@ app.controller("myCtrl", function ($scope) {
 
     //Update Section 1----------------------------------
     $scope.update1 = "UPSC EPFO 2023 Apply Online";
-    $scope.update1_link = "menu/lmenu/latest_job.html";
+    $scope.update1_link = "menu/demo.html";
 
     //Update Section 2----------------------------------
     $scope.update2 = "Haryana TGT 2023 Apply online";
-    $scope.update2_link = "menu/lmenu/latest_job.html";
+    $scope.update2_link = "menu/demo.html";
 
     //Update Section 3----------------------------------
     $scope.update3 = "CUET UG 2023 Apply Online";
-    $scope.update3_link = "menu/lmenu/latest_job.html";
+    $scope.update3_link = "menu/demo.html";
 
     //Update Section 4----------------------------------
     $scope.update4 = "NEET UG 2023 Apply Online";
-    $scope.update4_link = "menu/lmenu/latest_job.html";
+    $scope.update4_link = "menu/demo.html";
 
     //Update Section 5----------------------------------
     $scope.update5 = "NEET UG 2023 Apply Online";
-    $scope.update5_link = "menu/lmenu/latest_job.html";
+    $scope.update5_link = "menu/demo.html";
 
     //Update Section 6----------------------------------
     $scope.update6 = "UPPSC Pre 2023 Apply Online";
-    $scope.update6_link = "menu/lmenu/latest_job.html";
+    $scope.update6_link = "menu/demo.html";
 
     //-----------------Latest Update End--------------//
 
@@ -32,43 +32,43 @@ app.controller("myCtrl", function ($scope) {
 
     //Result Section 1----------------------------------
     $scope.result1 = "AFCAT 01/2023 Result";
-    $scope.result1_link = "menu/latest_job.html";
+    $scope.result1_link = "menu/demo.html";
 
     //Result Section 2----------------------------------
     $scope.result2 = "India Post GDS Result 2023";
-    $scope.result2_link = "menu/latest_job.html";
+    $scope.result2_link = "menu/demo.html";
 
     //Result Section 3----------------------------------
     $scope.result3 = "SBI Junior Associates Clerk 2022 Final Result";
-    $scope.result3_link = "menu/latest_job.html";
+    $scope.result3_link = "menu/demo.html";
 
     //Result Section 4----------------------------------
     $scope.result4 = "LIC AAO Phase I Pre Result 2023";
-    $scope.result4_link = "menu/latest_job.html";
+    $scope.result4_link = "menu/demo.html";
 
     //Result Section 5----------------------------------
     $scope.result5 = "SBI PO Phase II Main Result 2023";
-    $scope.result5_link = "menu/latest_job.html";
+    $scope.result5_link = "menu/demo.html";
 
     //Result Section 6----------------------------------
     $scope.result6 = "Supreme Court of India Junior Court Assistant Result, Score Card 2023";
-    $scope.result6_link = "menu/latest_job.html";
+    $scope.result6_link = "menu/demo.html";
 
     //Result Section 7----------------------------------
     $scope.result7 = "UPSSSC Assistant Boring Technician 2019 Result";
-    $scope.result7_link = "menu/latest_job.html";
+    $scope.result7_link = "menu/demo.html";
 
     //Result Section 8----------------------------------
     $scope.result8 = "UPSSSC Forest Guard Eligibility Result 2023";
-    $scope.result8_link = "menu/latest_job.html";
+    $scope.result8_link = "menu/demo.html";
 
     //Result Section 9----------------------------------
     $scope.result9 = "UPSC Geo Scientist 2023 Result";
-    $scope.result9_link = "menu/latest_job.html";
+    $scope.result9_link = "menu/demo.html";
 
     //Result Section 10----------------------------------
     $scope.result10 = "UPSC Geo Scientist 2023 Result";
-    $scope.result10_link = "menu/latest_job.html";
+    $scope.result10_link = "menu/demo.html";
 
     //------------------Result End---------------------//
 
@@ -76,43 +76,43 @@ app.controller("myCtrl", function ($scope) {
 
     //admit Section 1----------------------------------
     $scope.admit1 = "UPSSSC Forest Guard 2019 Document Verification Admit Card";
-    $scope.admit1_link = "menu/latest_job.html";
+    $scope.admit1_link = "menu/demo.html";
 
     //admit Section 2----------------------------------
     $scope.admit2 = "SGPGI Staff Nurse Admit Card 2023";
-    $scope.admit2_link = "menu/latest_job.html";
+    $scope.admit2_link = "menu/demo.html";
 
     //admit Section 3----------------------------------
     $scope.admit3 = "DRDO CEPTAM 10 Tech A Tier II Admit Card 2023";
-    $scope.admit3_link = "menu/latest_job.html";
+    $scope.admit3_link = "menu/demo.html";
 
     //admit Section 4----------------------------------
     $scope.admit4 = "ITBP Constable Animal Transport PET PST Admit Card 2023";
-    $scope.admit4_link = "menu/latest_job.html";
+    $scope.admit4_link = "menu/demo.html";
 
     //admit Section 5----------------------------------
     $scope.admit5 = "ITBP Constable Pioneer PET PST Admit Card 2023";
-    $scope.admit5_link = "menu/latest_job.html";
+    $scope.admit5_link = "menu/demo.html";
 
     //admit Section 6----------------------------------
     $scope.admit6 = "ITBP Assistant Commandant Transport PET PST Admit Card 2023";
-    $scope.admit6_link = "menu/latest_job.html";
+    $scope.admit6_link = "menu/demo.html";
 
     //admit Section 7----------------------------------
     $scope.admit7 = "ITBP HC Telecommunication PET PST Admit Card 2023";
-    $scope.admit7_link = "menu/latest_job.html";
+    $scope.admit7_link = "menu/demo.html";
 
     //admit Section 8----------------------------------
     $scope.admit8 = "ITBP HC Motor Mechanic PET PST Admit Card 2023";
-    $scope.admit8_link = "menu/latest_job.html";
+    $scope.admit8_link = "menu/demo.html";
 
     //admit Section 9----------------------------------
     $scope.admit9 = "ITBP HC Dresser PET PST Admit Card 2023";
-    $scope.admit9_link = "menu/latest_job.html";
+    $scope.admit9_link = "menu/demo.html";
 
     //admit Section 10----------------------------------
     $scope.admit10 = "ITBP ASI Pharmacist PET PST Admit Card 2023";
-    $scope.admit10_link = "menu/latest_job.html";
+    $scope.admit10_link = "menu/demo.html";
 
     //------------------Admit Card End---------------//
 
@@ -120,43 +120,43 @@ app.controller("myCtrl", function ($scope) {
 
     //job Section 1----------------------------------
     $scope.job1 = "NTA CSIR UGC NET Online Form June 2023";
-    $scope.job1_link = "menu/latest_job.html";
+    $scope.job1_link = "menu/demo.html";
 
     //job Section 2----------------------------------
     $scope.job2 = "AAICLAS Security Screener Online Form 2023";
-    $scope.job2_link = "menu/latest_job.html";
+    $scope.job2_link = "menu/demo.html";
 
     //job Section 3----------------------------------
     $scope.job3 = "NIC Scientific and Technical Posts Online Form 2023";
-    $scope.job3_link = "menu/latest_job.html";
+    $scope.job3_link = "menu/demo.html";
 
     //job Section 4----------------------------------
     $scope.job4 = "MP Apex Bank Various Post Online Form 2023";
-    $scope.job4_link = "menu/latest_job.html";
+    $scope.job4_link = "menu/demo.html";
 
     //job Section 5----------------------------------
     $scope.job5 = "NTA JNU Non Teaching Various Post Online Form 2023";
-    $scope.job5_link = "menu/latest_job.html";
+    $scope.job5_link = "menu/demo.html";
 
     //job Section 6----------------------------------
     $scope.job6 = "MPESB Group 4 AG 3 and Other Post Online Form 2023";
-    $scope.job6_link = "menu/latest_job.html";
+    $scope.job6_link = "menu/demo.html";
 
     //job Section 7----------------------------------
     $scope.job7 = "SSC Selection Post XI Online Form 2023";
-    $scope.job7_link = "menu/latest_job.html";
+    $scope.job7_link = "menu/demo.html";
 
     //job Section 8----------------------------------
     $scope.job8 = "Delhi High Court Personal Assistant Online Form 2023";
-    $scope.job8_link = "menu/latest_job.html";
+    $scope.job8_link = "menu/demo.html";
 
     //job Section 9----------------------------------
     $scope.job9 = "Delhi High Court Personal Assistant Online Form 2023";
-    $scope.job9_link = "menu/latest_job.html";
+    $scope.job9_link = "menu/demo.html";
 
     //job Section 10----------------------------------
     $scope.job10 = "UPPSC Pre 2023 Online Form";
-    $scope.job10_link = "menu/latest_job.html";
+    $scope.job10_link = "menu/demo.html";
 
     //----------------Latest Job End-----------------//
 
@@ -164,31 +164,31 @@ app.controller("myCtrl", function ($scope) {
 
     //answer Section 1----------------------------------
     $scope.answer1 = "MP High School TET Answer Key 2023";
-    $scope.answer1_link = "menu/latest_job.html";
+    $scope.answer1_link = "menu/demo.html";
 
     //answer Section 2----------------------------------
     $scope.answer2 = "Bihar Board BSEB Class 12th Annual Exam Answer Key 2023";
-    $scope.answer2_link = "menu/latest_job.html";
+    $scope.answer2_link = "menu/demo.html";
 
     //answer Section 3----------------------------------
     $scope.answer3 = "MPESB Excise Constable Answer Key 2023";
-    $scope.answer3_link = "menu/latest_job.html";
+    $scope.answer3_link = "menu/demo.html";
 
     //answer Section 4----------------------------------
     $scope.answer4 = "KVS TGT,PGT, Other Post Answer Key 2023";
-    $scope.answer4_link = "menu/latest_job.html";
+    $scope.answer4_link = "menu/demo.html";
 
     //answer Section 5----------------------------------
     $scope.answer5 = "IIT GATE 2023 Answer Key";
-    $scope.answer5_link = "menu/latest_job.html";
+    $scope.answer5_link = "menu/demo.html";
 
     //answer Section 6----------------------------------
     $scope.answer6 = "BPSC 68th Pre Answer Key 2023";
-    $scope.answer6_link = "menu/latest_job.html";
+    $scope.answer6_link = "menu/demo.html";
 
     //answer Section 7----------------------------------
     $scope.answer7 = "SSC GD Constable 2022 Answer Key";
-    $scope.answer7_link = "menu/latest_job.html";
+    $scope.answer7_link = "menu/demo.html";
 
     //------------------Answer Key End-------------------//
 
@@ -196,31 +196,31 @@ app.controller("myCtrl", function ($scope) {
 
     //syllabus Section 1----------------------------------
     $scope.syllabus1 = "UPSSSC Forest Guard Syllabus 2023";
-    $scope.syllabus1_link = "menu/latest_job.html";
+    $scope.syllabus1_link = "menu/demo.html";
 
     //syllabus Section 2----------------------------------
     $scope.syllabus2 = "SSC MTS and Havaldar Syllabus 2023";
-    $scope.syllabus2_link = "menu/latest_job.html";
+    $scope.syllabus2_link = "menu/demo.html";
 
     //syllabus Section 3----------------------------------
     $scope.syllabus3 = "Rajasthan State Eligibility Test SET Syllabus 2023";
-    $scope.syllabus3_link = "menu/latest_job.html";
+    $scope.syllabus3_link = "menu/demo.html";
 
     //syllabus Section 4----------------------------------
     $scope.syllabus4 = "Rajasthan State Eligibility Test SET Syllabus 2023";
-    $scope.syllabus4_link = "menu/latest_job.html";
+    $scope.syllabus4_link = "menu/demo.html";
 
     //syllabus Section 5----------------------------------
     $scope.syllabus5 = "MPESB High School TET Syllabus 2023";
-    $scope.syllabus5_link = "menu/latest_job.html";
+    $scope.syllabus5_link = "menu/demo.html";
 
     //syllabus Section 6----------------------------------
     $scope.syllabus6 = "SSC 10+2 CHSL Syllabus 2022";
-    $scope.syllabus6_link = "menu/latest_job.html";
+    $scope.syllabus6_link = "menu/demo.html";
 
     //syllabus Section 7----------------------------------
     $scope.syllabus7 = "BPSC Drug Inspector Syllabus 2022";
-    $scope.syllabus7_link = "menu/latest_job.html";
+    $scope.syllabus7_link = "menu/demo.html";
 
     //------------------syllabus End---------------------//
 
@@ -228,31 +228,31 @@ app.controller("myCtrl", function ($scope) {
 
     //adm Section 1----------------------------------
     $scope.adm1 = "NTA CUET UG 2023 Online Form";
-    $scope.adm1_link = "menu/latest_job.html";
+    $scope.adm1_link = "menu/demo.html";
 
     //adm Section 2----------------------------------
     $scope.adm2 = "NTA NEET UG 2023 Online Form";
-    $scope.adm2_link = "menu/latest_job.html";
+    $scope.adm2_link = "menu/demo.html";
 
     //adm Section 3----------------------------------
     $scope.adm3 = "UP Polytechnic JEECUP Online Form 2023";
-    $scope.adm3_link = "menu/latest_job.html";
+    $scope.adm3_link = "menu/demo.html";
 
     //adm Section 4----------------------------------
     $scope.adm4 = "Rajju Bhaiya University PRSU PHd Admission DET Online Form 2023";
-    $scope.adm4_link = "menu/latest_job.html";
+    $scope.adm4_link = "menu/demo.html";
 
     //adm Section 5----------------------------------
     $scope.adm5 = "BHU SET 2023 Online Form";
-    $scope.adm5_link = "menu/latest_job.html";
+    $scope.adm5_link = "menu/demo.html";
 
     //adm Section 6----------------------------------
     $scope.adm6 = "Bihar B.Ed 2023 Online Form";
-    $scope.adm6_link = "menu/latest_job.html";
+    $scope.adm6_link = "menu/demo.html";
 
     //adm Section 7----------------------------------
     $scope.adm7 = "Allahabad University CRET Online Form 2023";
-    $scope.adm7_link = "menu/latest_job.html";
+    $scope.adm7_link = "menu/demo.html";
 
     //------------------Admission End---------------------//
 
@@ -260,23 +260,23 @@ app.controller("myCtrl", function ($scope) {
 
     //comingExam Section 1----------------------------------
     $scope.comingExam1 = "DSSSB TGT PGT PRT Exam";
-    $scope.comingExam1_link = "menu/latest_job.html";
+    $scope.comingExam1_link = "menu/demo.html";
 
     //comingExam Section 2----------------------------------
     $scope.comingExam2 = "SSC CHSL (Mains)";
-    $scope.comingExam2_link = "menu/latest_job.html";
+    $scope.comingExam2_link = "menu/demo.html";
 
     //comingExam Section 3----------------------------------
     $scope.comingExam3 = "SSC JE (Tier-1)";
-    $scope.comingExam3_link = "menu/latest_job.html";
+    $scope.comingExam3_link = "menu/demo.html";
 
     //comingExam Section 4----------------------------------
     $scope.comingExam4 = "SOSSSC Nursing Officer Exam";
-    $scope.comingExam4_link = "menu/latest_job.html";
+    $scope.comingExam4_link = "menu/demo.html";
 
     //comingExam Section 5----------------------------------
     $scope.comingExam5 = "SSC Stenographer";
-    $scope.comingExam5_link = "menu/latest_job.html";
+    $scope.comingExam5_link = "menu/demo.html";
 
     //------------------Up Coming Exam End---------------------//
 
@@ -284,23 +284,23 @@ app.controller("myCtrl", function ($scope) {
 
     //importantlink Section 1----------------------------------
     $scope.importantlink1 = "MPESB Profile Online Registration 2023";
-    $scope.importantlink1_link = "menu/latest_job.html";
+    $scope.importantlink1_link = "menu/demo.html";
 
     //importantlink Section 2----------------------------------
     $scope.importantlink2 = "Har Ghar Tiranga Abhiyan Online Registration 2023";
-    $scope.importantlink2_link = "menu/latest_job.html";
+    $scope.importantlink2_link = "menu/demo.html";
 
     //importantlink Section 3----------------------------------
     $scope.importantlink3 = "E Sharam Card Online Registration 2023";
-    $scope.importantlink3_link = "menu/latest_job.html";
+    $scope.importantlink3_link = "menu/demo.html";
 
     //importantlink Section 4----------------------------------
     $scope.importantlink4 = "E Sharam Card Online Registration 2023";
-    $scope.importantlink4_link = "menu/latest_job.html";
+    $scope.importantlink4_link = "menu/demo.html";
 
     //importantlink Section 5----------------------------------
     $scope.importantlink5 = "Aadhar Card Download | Appointment Book | Update | PVC Order | Etc 2023";
-    $scope.importantlink5_link = "menu/latest_job.html";
+    $scope.importantlink5_link = "menu/demo.html";
 
     //------------------Importants link End---------------------//
 });
